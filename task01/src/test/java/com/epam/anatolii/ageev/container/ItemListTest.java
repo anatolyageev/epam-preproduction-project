4c481947c7ebe0f3dc999aa7b1a052634e0bbd10
Anatolii Agieiev <anatolii_agieiev@epam.com>
2020-07-16 14:41:14 +0300

    Magic numbers were replaced

--- task01/src/test/java/com/epam/anatolii/ageev/container/ItemListTest.java
+++ task01/src/test/java/com/epam/anatolii/ageev/container/ItemListTest.java
@@ -15,58 +15,60 @@ public class ItemListTest {
     private ItemList<Item> itemList;
 
     @Before
-    public void before(){
+    public void before() {
         itemList = new ItemList<>();
-        itemList.add(new Server(1L,1000.00,"Intel core 9",4.5,48,"FX",2,true));
-        itemList.add(new Server(2L,1000.00,"Intel core 9",4.5,48,"FX",2,true));
-        itemList.add(new Server(3L,1003.00,"Intel core 9",4.5,48,"FX",2,true));
-        itemList.add(new Server(4L,1000.00,"Intel core 9",4.5,48,"FX",2,true));
-        itemList.add(new Server(5L,1003.00,"Intel core 9",4.5,48,"FX",2,true));
-        itemList.add(new Server(6L,1000.00,"Intel core 9",4.5,48,"FX",2,true));
-        itemList.add(new Server(7L,1000.00,"Intel core 9",4.5,48,"FX",2,true));
+        itemList.add(new Server(1L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true));
+        itemList.add(new Server(2L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true));
+        itemList.add(new Server(3L, 1003.00, "Intel core 9", 4.5, 48, "FX", 2, true));
+        itemList.add(new Server(4L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true));
+        itemList.add(new Server(5L, 1003.00, "Intel core 9", 4.5, 48, "FX", 2, true));
+        itemList.add(new Server(6L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true));
+        itemList.add(new Server(7L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true));
     }
 
     @Test
     public void addNewItem_returnTrue() {
-        assertTrue(itemList.add(new Server(1L,1000.00,"Intel core 9",4.5,48,"FX",2,true)));
+        assertTrue(itemList.add(new Server(1L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true)));
     }
 
     @Test
     public void addNewItemByIndex_returnTrue() {
-       Item item = new Server(8L,1000.00,"Intel core 9",4.5,48,"FX",2,true);
-       itemList.add(6,item);
-       assertTrue(itemList.contains(item));
+        Item item = new Server(8L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true);
+        itemList.add(6, item);
+        assertTrue(itemList.contains(item));
     }
 
     @Test
-    public void getItem_returnTrue(){
-        Item item = new Server(8L,1000.00,"Intel core 9",4.5,48,"FX",2,true);
-        itemList.add(6,item);
-        assertTrue(itemList.get(6)==item);
+    public void getItem_returnTrue() {
+        Item item = new Server(8L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true);
+        itemList.add(6, item);
+        assertTrue(itemList.get(6) == item);
     }
 
     @Test
-    public void removeByIndex_shouldRemainSixItems(){
+    public void removeByIndex_shouldRemainSixItems() {
+        int beforeSize = itemList.size();
         itemList.remove(0);
-        assertTrue(itemList.size()==6);
+        assertTrue(itemList.size() == beforeSize-1);
     }
 
     @Test
-    public void removeByObject_shouldRemainFiveItems(){
-        itemList.remove(new Server(7L,1000.00,"Intel core 9",4.5,48,"FX",2,true));
-        assertTrue(itemList.size()==6);
+    public void removeByObject_shouldRemainFiveItems() {
+        int beforeSize = itemList.size();
+        itemList.remove(new Server(7L, 1000.00, "Intel core 9", 4.5, 48, "FX", 2, true));
+        assertTrue(itemList.size() == beforeSize - 1);
     }
 
     @Test
-    public void customIteratorTest_resultListShouldContainTwoElement(){
-        Iterator<Item> iterator= itemList.iteratorCustom(item -> item.getPrice()==1003.00);
+    public void customIteratorTest_resultListShouldContainTwoElement() {
+        Iterator<Item> iterator = itemList.iteratorCustom(item -> item.getPrice() == 1003.00);
         List<Item> testItemList = new ItemList<>();
 
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             testItemList.add(iterator.next());
         }
 
-        assertTrue(testItemList.size()==2);
+        assertTrue(testItemList.size() == 2);
     }
 
 }
